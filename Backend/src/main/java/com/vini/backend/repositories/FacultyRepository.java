80be08cdc6534ad09d2e801de939895f7ecc4fc7
Vinith11 <vinith2003@gmail.com>
2024-07-31 23:52:03 +0530

    Completed and Tested Authentication with Error Handling

--- Backend/src/main/java/com/vini/backend/repositories/FacultyRepository.java
+++ Backend/src/main/java/com/vini/backend/repositories/FacultyRepository.java
@@ -5,6 +5,12 @@ import com.vini.backend.models.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FacultyRepository extends JpaRepository<Faculty, String> {
+    public Faculty findByFacultyEmail(String email);
+
+    //public List<Faculty> findAllByOrderByCreatedAtDesc();
+
 }
--- Backend/src/main/java/com/vini/backend/repositories/StudentRepository.java
+++ Backend/src/main/java/com/vini/backend/repositories/StudentRepository.java
@@ -5,6 +5,12 @@ import com.vini.backend.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+    public Student findByStudentEmail(String email);
+
+    //public List<Student> findAllByOrderByCreatedAtDesc();
+
 }
